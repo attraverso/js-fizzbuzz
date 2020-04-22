1d992c9d89f722381e2db01712e8e02c2304efef
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-22 16:36:32 +0200

    can't get count_display to act like string

--- main.js
+++ main.js
@@ -9,28 +9,36 @@ var counting_to = 100;
 for (var i = 0; i < counting_to; i++) {
 
   var current_count = (i + 1);
-  var count_display = current_count;
-  console.log(count_display);
+  console.log('current count: ', + current_count);
 
-  /*mark multiples of 3 and switch with 'fizz'*/
+  /*mark multiples of 3*/
   var remainder3 = current_count % 3;
-  console.log('resto: ' + remainder3);
+  console.log('resto 3: ' + remainder3);
 
+  /*mark multiples of 5 and switch with 'fizz'*/
+  var remainder5 = current_count % 5;
+  console.log('resto 5: ' + remainder5);
+
+  /*define second set of numbers you can tamper with w/o the calculations being affected */
+  var count_display = current_count.toString();
+  console.log('count display: ', + count_display);
+
+  /* switch multiples of 3 with 'fizz'*/
   if (remainder3 == 0) {
     console.log(count_display = 'fizz');
   }
 
-/*mark multiples of 5 and switch with 'fizz'*/
-var remainder5 = current_count % 5;
-console.log('resto: ' + remainder5);
-
-if (remainder5 == 0) {
-  console.log(count_display = 'buzz');
+  /* switch multiples of 5 with 'buzz'*/
+  if (remainder5 == 0) {
+    console.log(count_display = 'buzz');
 }
 
+  /* switch multiples of 3 and 5 with 'fizzbuzz'*/
   if ((remainder3 == 0) && (remainder5 == 0)) {
     console.log(count_display = 'fizzbuzz');
   }
+
+  console.log('count display2: ', + count_display);
 
 }
 
